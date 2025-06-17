d80e2d169999882e0ddcd49706bca97ee6b71e42
Manikanta-81 <vaddimanikanta2002@gmail.com>
2025-06-17 13:02:24 +0530

    Removed unwanted logo and Some changes

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -8,17 +8,16 @@ const {
   getBookingById,
 } = require("../controllers/bookingControllers");
 
-
-const  authenticateToken=require("../middlewares/userMiddleware");
+const authenticateToken = require("../middlewares/userMiddleware");
 
 //  POST route to create a new booking
-router.post("/create-booking",authenticateToken, createBooking);
+router.post("/create-booking", authenticateToken, createBooking);
 
 // GET route to fetch all bookings
 router.get("/get-bookings", getAllBookings);
 
 router.get("/get-booking-by-id/:bookingId", getBookingById);
 
-router.put("/confirm-booking/:bookingId",authenticateToken, confirmBooking);
+router.put("/confirm-booking/:bookingId", authenticateToken, confirmBooking);
 
 module.exports = router;
